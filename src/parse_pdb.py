823949447d42edb849532227be73e345f05a0056
sophielem <lematre.sophie@gmail.com>
2018-11-14 19:35:10 +0100

    add parse and write function pdb

---
+++ src/parse_pdb.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+import math, string
+
+
+def parsePDBMultiChains(infile, charge = 1, chargeFromInfile = False, bfactor = False, CG = False) :
+    """ purpose: to parse a pdb file (infile)
+        input: PDB file
+        output: a dico dPDB which contains for each atom of each residue of
+        each chain, its corresponding 3D coordinates. Please take a look to
+        the code to understand the structure of the dico.
+    """
+
+    # lecture du fichier PDB
+    f = open(infile, "r")
+    lines = f.readlines()
+    f.close()
+
+
+    # var init
+    chaine = True
+    firstline = True
+    prevres = None
+    dPDB = {}
+    dPDB["reslist"] = []
+    current_res = 1
+    prev_res = None
+    # parcoure le PDB
+    for line in lines :
+        if (line[0:4] == "ATOM") or ((line[0:6] == "HETATM") and ( (line[17:20].strip() == "MET") or  (line[17:20].strip() == "MSE") )) :
+            code_res = str(current_res) + line[26:27].strip()
+            cures = line[22:27].strip()
+            if(cures != prev_res and prev_res != None) :  current_res += 1
+            if not code_res in dPDB["reslist"] : # first time we encounter it
+                dPDB["reslist"].append(code_res)
+                dPDB[code_res] = {}
+                dPDB[code_res]["resname"] = line[17:20].strip()
+                dPDB[code_res]["atomlist"] = []
+                #dPDB[curres]["atomlistTowrite"] = []
+                alternateoccupancy = None #"%s"%(line[16:17])
+                occupancy = "%s"%(line[16:17])
+                if occupancy != " " :
+                    alternateoccupancy = occupancy
+
+
+            else: # this is not a new residue
+                occupancy = "%s"%(line[16:17])
+
+                if occupancy != " " and alternateoccupancy == None : # means we are in the first alternate location of that residue
+                    alternateoccupancy = occupancy
+
+            if CG : # means we are parsing a CG model so we have to treat the CSE atomtypes which can be redondant in terms of name the same res
+                atomtype = "%s_%s"%(line[6:11].strip(), line[12:16].strip())
+            else:
+                atomtype = line[12:16].strip()
+
+            #if not atomtype in dPDB[curres]["atomlist"] :
+            if occupancy == alternateoccupancy  or occupancy == " " : # means this atom corresponds to the first rotamer found in the PDB for this residue
+
+                #if CG :
+                    #dPDB[curres]["atomlistTowrite"].append(atomtype.split("_")[1]) # necessary for the writing later
+
+                dPDB[code_res]["atomlist"].append(atomtype)
+                dPDB[code_res][atomtype] = {}
+                dPDB[code_res][atomtype]["x"] = float(line[30:38])
+                dPDB[code_res][atomtype]["y"] = float(line[38:46])
+                dPDB[code_res][atomtype]["z"] = float(line[46:54])
+                dPDB[code_res][atomtype]["id"] = line[6:11].strip()
+                if bfactor == True :
+                    dPDB[curres][atomtype]["bfactor"] = float(line[60:67].strip())
+                #if chargeFromInfile == True :
+                #    dPDB[curres][atomtype]["charge"] = float(line[60:67])
+                #else :
+                #    dPDB[curres][atomtype]["charge"] = charge
+
+
+            dPDB[code_res]["resnum"] = current_res
+            prev_res = cures
+            #dPDB[curres]["inser"] =  "%s"%(line[26:27])
+
+
+    return dPDB
+
+
+def writePDB(dPDB, filout = "out.pdb", bfactor = False) :
+    """purpose: according to the coordinates in dPDB, writes the corresponding PDB file.
+       If bfactor = True, writes also the information corresponding to the key bfactor
+       of each residue (one key per residue) in dPDB.
+       input: a dico with the dPDB format
+       output: PDB file.
+    """
+
+    fout = open(filout, "w")
+
+    for res in dPDB["reslist"] :
+        for atom in dPDB[res]["atomlist"] :
+            if bfactor :
+                fout.write("ATOM  %5s  %-4s%3s %s%4s    %8.3f%8.3f%8.3f  1.00%7.3f X X\n"%(dPDB[res][atom]["id"], atom, dPDB[res]["resname"],chain, res,dPDB[res][atom]["x"], dPDB[res][atom]["y"],dPDB[res][atom]["z"],dPDB[res]["bfactor"] ))
+            else:
+                fout.write("ATOM  %5s  %-4s%3s %s%4s    %8.3f%8.3f%8.3f  1.00  1.00 X X\n"%(dPDB[res][atom]["id"], atom, dPDB[res]["resname"],chain, res,dPDB[res][atom]["x"], dPDB[res][atom]["y"],dPDB[res][atom]["z"] ))
+
+    fout.close()
