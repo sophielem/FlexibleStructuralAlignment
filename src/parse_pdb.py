1fa8263726f559753d5b03535dcaf44da1ee3cf5
sophielem <lematre.sophie@gmail.com>
2018-11-15 10:03:43 +0100

    write pdb by start and end

--- src/parse_pdb.py
+++ src/parse_pdb.py
@@ -3,24 +3,19 @@
 import math, string
 
 
-def parsePDBMultiChains(infile, charge = 1, chargeFromInfile = False, bfactor = False, CG = False) :
+def parsePDBMultiChains(infile, charge = 1, chargeFromInfile = False, bfactor = True, CG = False) :
     """ purpose: to parse a pdb file (infile)
         input: PDB file
         output: a dico dPDB which contains for each atom of each residue of
         each chain, its corresponding 3D coordinates. Please take a look to
         the code to understand the structure of the dico.
     """
-
     # lecture du fichier PDB
     f = open(infile, "r")
     lines = f.readlines()
     f.close()
 
-
     # var init
-    chaine = True
-    firstline = True
-    prevres = None
     dPDB = {}
     dPDB["reslist"] = []
     current_res = 1
@@ -42,7 +37,6 @@ def parsePDBMultiChains(infile, charge = 1, chargeFromInfile = False, bfactor = 
                 if occupancy != " " :
                     alternateoccupancy = occupancy
 
-
             else: # this is not a new residue
                 occupancy = "%s"%(line[16:17])
 
@@ -57,9 +51,6 @@ def parsePDBMultiChains(infile, charge = 1, chargeFromInfile = False, bfactor = 
             #if not atomtype in dPDB[curres]["atomlist"] :
             if occupancy == alternateoccupancy  or occupancy == " " : # means this atom corresponds to the first rotamer found in the PDB for this residue
 
-                #if CG :
-                    #dPDB[curres]["atomlistTowrite"].append(atomtype.split("_")[1]) # necessary for the writing later
-
                 dPDB[code_res]["atomlist"].append(atomtype)
                 dPDB[code_res][atomtype] = {}
                 dPDB[code_res][atomtype]["x"] = float(line[30:38])
@@ -67,22 +58,16 @@ def parsePDBMultiChains(infile, charge = 1, chargeFromInfile = False, bfactor = 
                 dPDB[code_res][atomtype]["z"] = float(line[46:54])
                 dPDB[code_res][atomtype]["id"] = line[6:11].strip()
                 if bfactor == True :
-                    dPDB[curres][atomtype]["bfactor"] = float(line[60:67].strip())
-                #if chargeFromInfile == True :
-                #    dPDB[curres][atomtype]["charge"] = float(line[60:67])
-                #else :
-                #    dPDB[curres][atomtype]["charge"] = charge
-
+                    dPDB[code_res][atomtype]["bfactor"] = float(line[60:67].strip())
 
             dPDB[code_res]["resnum"] = current_res
             prev_res = cures
-            #dPDB[curres]["inser"] =  "%s"%(line[26:27])
 
 
     return dPDB
 
 
-def writePDB(dPDB, filout = "out.pdb", bfactor = False) :
+def writePDB(dPDB, filout, bfactor, start, end) :
     """purpose: according to the coordinates in dPDB, writes the corresponding PDB file.
        If bfactor = True, writes also the information corresponding to the key bfactor
        of each residue (one key per residue) in dPDB.
@@ -93,10 +78,11 @@ def writePDB(dPDB, filout = "out.pdb", bfactor = False) :
     fout = open(filout, "w")
 
     for res in dPDB["reslist"] :
-        for atom in dPDB[res]["atomlist"] :
-            if bfactor :
-                fout.write("ATOM  %5s  %-4s%3s %s%4s    %8.3f%8.3f%8.3f  1.00%7.3f X X\n"%(dPDB[res][atom]["id"], atom, dPDB[res]["resname"],chain, res,dPDB[res][atom]["x"], dPDB[res][atom]["y"],dPDB[res][atom]["z"],dPDB[res]["bfactor"] ))
-            else:
-                fout.write("ATOM  %5s  %-4s%3s %s%4s    %8.3f%8.3f%8.3f  1.00  1.00 X X\n"%(dPDB[res][atom]["id"], atom, dPDB[res]["resname"],chain, res,dPDB[res][atom]["x"], dPDB[res][atom]["y"],dPDB[res][atom]["z"] ))
+        if dPDB[res]["resnum"] >= start + 1 and dPDB[res]["resnum"] <= end + 1:
+            for atom in dPDB[res]["atomlist"] :
+                if bfactor :
+                    fout.write("ATOM  %5s  %-4s%3s %s%4s    %8.3f%8.3f%8.3f  1.00%7.3f X X\n"%(dPDB[res][atom]["id"], atom, dPDB[res]["resname"],"A", res,dPDB[res][atom]["x"], dPDB[res][atom]["y"],dPDB[res][atom]["z"],dPDB[res][atom]["bfactor"] ))
+                else:
+                    fout.write("ATOM  %5s  %-4s%3s %s%4s    %8.3f%8.3f%8.3f  1.00  1.00 X X\n"%(dPDB[res][atom]["id"], atom, dPDB[res]["resname"],"A", res,dPDB[res][atom]["x"], dPDB[res][atom]["y"],dPDB[res][atom]["z"] ))
 
     fout.close()
--- src/test_tmalign.py
+++ src/test_tmalign.py
@@ -23,16 +23,16 @@ m = re.search("TM-score *= (?P<score>[0-9]*\.[0-9]*)", text)
 print("TMscore Score normalized by length of the second chain {}".format(m.group("score")))
 
 os.system("dssp data/1aoh.pdb > data/1aoh.dss")
-os.system("bin/./peeling11_4.1 -pdb data/1aoh.pdb -dssp data/1aoh.dss -R2 95 -ss2 8 -lspu 20 -mspu 0 -d0 6.0 -delta 1.5 -oss 0 -p 0 -cp 0 -npu 16 > results/peeling_output.txt")
+os.system("bin/./peeling11_4.1 -pdb data/1aoh.pdb -dssp data/1aoh.dss -R2 95 -ss2 8 -lspu 20 -mspu 0 -d0 6.0 -delta 1.5 -oss 1 -p 0 -cp 0 -npu 16 > results/peeling_output.txt")
 with open("results/peeling_output.txt", "r") as filin:
     text = filin.read().splitlines()
 text = text[15 : ]
 text = [itx.split()[4: ] for itx in text]
+dPDB = parse.parsePDBMultiChains("data/1aoh.pdb")
 for itx in text:
     nb_pu = int(itx[0])
-    for i in range(1, nb_pu+2, 2):
-        start = itx[i]
-        end = itx[i + 1]
-        dPDB = parse.parsePDBMultiChains("data/1aoh.pdb")
-        parse.writePDB(dPDB, filout = "data/fou.pdb")
-        break
+    for i in range(1, nb_pu*2, 2):
+        start = int(itx[i])
+        end = int(itx[i + 1])
+        parse.writePDB(dPDB, ("data/" + str(nb_pu) + "_" + str(i) + ".pdb"), True, start, end)
+break
