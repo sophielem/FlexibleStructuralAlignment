81162cae283c4513c42da2d6a89e71cb075d51db
sophielem <lematre.sophie@gmail.com>
2018-11-16 14:01:04 +0100

    pylint

--- main.py
+++ main.py
@@ -12,31 +12,35 @@ Options:
   -p --input2 = file2        input2 pdb
 """
 
-import docopt
 import os
+import sys
+import docopt
 import src.flexible_alignment as flex
 
+
 if __name__ == '__main__':
-    arg = docopt.docopt(__doc__, version='0.1')
-    input1 = arg["--input"]
-    input2 = arg["--input2"]
-    if not(os.path.isfile(input1) and os.path.isfile(input2)):
+    ARG = docopt.docopt(__doc__, version='0.1')
+    INPUT1 = ARG["--input"]
+    INPUT2 = ARG["--input2"]
+    if not(os.path.isfile(INPUT1) and os.path.isfile(INPUT2)):
         print("The file doesn't exist !")
         sys.exit(2)
 
-    os.system("dssp " + input1 + " > data/input1.dss")
+    os.system("dssp " + INPUT1 + " > data/input1.dss")
 
     # Protein peeling
-    cmd_line = ("bin/./peeling11_4.1 -pdb " + input1 + " -dssp data/input1.dss -R2 95 -ss2 8 -lspu 20 -mspu 0 -d0 6.0 -delta 1.5 -oss 1 -p 0 -cp 0 -npu 16")
-    output = flex.call_executabe(cmd_line).split("\n")
-    flex.parse_protein_peeling(output)
+    CMD_LINE = ("bin/./peeling11_4.1 -pdb " + INPUT1 +
+                " -dssp data/input1.dss -R2 95 -ss2 8 -lspu 20 -mspu 0 \
+                -d0 6.0 -delta 1.5 -oss 1 -p 0 -cp 0 -npu 16")
+    OUTPUT = flex.call_executabe(CMD_LINE).split("\n")
+    flex.parse_protein_peeling(OUTPUT)
 
     # TMalign
-    cmd_line = ("bin/./TMalign " + input1 + " " + input2 + " -o TM.sup")
-    output = flex.call_executabe(cmd_line)
-    sc_tmalign = flex.parse_tmscore(output, "TMalign")
+    CMD_LINE = ("bin/./TMalign " + INPUT1 + " " + INPUT2 + " -o TM.sup")
+    OUTPUT = flex.call_executabe(CMD_LINE)
+    SC_TMALIGN = flex.parse_tmscore(OUTPUT, "TMalign")
 
     #TMscore
-    cmd_line = ("bin/./TMscore " + input1 + " " + input2 + " -o TM.sup")
-    output = flex.call_executabe(cmd_line)
-    sc_tmscore = flex.parse_tmscore(output, "TMscore")
+    CMD_LINE = ("bin/./TMscore " + INPUT1 + " " + INPUT2 + " -o TM.sup")
+    OUTPUT = flex.call_executabe(CMD_LINE)
+    SC_TMSCORE = flex.parse_tmscore(OUTPUT, "TMscore")
