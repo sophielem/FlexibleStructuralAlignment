ada96cd319fc0e9fb166485310aedf7c3817acb6
sophielem <sophie.lematre@yahoo.com>
2018-11-18 15:21:15 +0100

    alignment on the 2 proteins

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ Options:
 
 import os
 import sys
-import copy
 import docopt
 import src.flexible_alignment as flex
 
@@ -27,39 +26,5 @@ if __name__ == '__main__':
         print("The file doesn't exist !")
         sys.exit(2)
 
-    # os.system("dssp " + INPUT1 + " > data/input1.dss")
-
-    # Protein peeling
-    CMD_LINE = ("bin/./peeling11_4.1 -pdb " + INPUT1 +
-                " -dssp data/1aoh.dss -R2 95 -ss2 8 -lspu 20 -mspu 0 \
-                -d0 6.0 -delta 1.5 -oss 1 -p 0 -cp 0 -npu 16")
-    OUTPUT = flex.call_executabe(CMD_LINE).split("\n")
-    dict_pu = flex.parse_protein_peeling(OUTPUT)
-    with open(INPUT2, "r") as filin:
-        text = filin.readlines()
-
-
-
-    dict_tmscore = {}
-    # flex.remove_aligned_region(input_example, "1")
-    for nb_pu in dict_pu:
-        nb_pu = 10
-        input_example = "input2.pdb"
-        with open(input_example, "w") as filout:
-            for line in text:
-                filout.write(line)
-        print(nb_pu)
-        flex.tm_align(copy.deepcopy(dict_pu), nb_pu, input_example)
-        #TMscore
-        CMD_LINE = ("bin/./TMscore test_aligned.pdb " + INPUT2 + " -o TM.sup")
-        OUTPUT = flex.call_executabe(CMD_LINE)
-        dict_tmscore[nb_pu] = flex.parse_tmscore(OUTPUT, "TMscore")
-        break
-        f = open('test_aligned.pdb', 'r+')
-        f.truncate(0)
-        f.close()
-
-
-
-    for clef in dict_tmscore:
-        print(dict_tmscore[clef])
+    DICT_TMSCORE_1_2 = flex.main_flex_aln(INPUT1, INPUT2)
+    DICT_TMSCORE_2_1 = flex.main_flex_aln(INPUT2, INPUT1)
--- src/flexible_alignment.py
+++ src/flexible_alignment.py
@@ -8,8 +8,10 @@ import subprocess as sub
 import re
 import os
 import glob
+import copy
 import src.parse_pdb as parse
 
+DEBUG = False
 
 def call_executabe(cmd_line):
     """
@@ -21,7 +23,7 @@ def call_executabe(cmd_line):
         the ouput of the soft
     """
     # Command to communicate with the shell and retrieve the output
-    out, err = sub.Popen(cmd_line.split(), stdout=sub.PIPE).communicate()
+    out = sub.Popen(cmd_line.split(), stdout=sub.PIPE).communicate()[0]
 
     # To remove all uncessary files created
     for filename in glob.glob("file*"):
@@ -77,8 +79,9 @@ def parse_tmscore(output, soft):
     if matches == []:
         return None
     else:
-        print("{} Score normalized by length of the second chain \
-               {}".format(soft, matches[index]))
+        if DEBUG:
+            print("{} Score normalized by length of the second chain \
+                   {}".format(soft, matches[index]))
         return float(matches[index])
 
 
@@ -95,7 +98,8 @@ def tm_align(dict_pu, nb_pu, input2):
 
     """
     # All PU are aligned
-    if dict_pu[nb_pu] == []: return None
+    if dict_pu[nb_pu] == []:
+        return None
     # Initialize a minus score
     score = [-100, 0]
     for i in dict_pu[nb_pu]:
@@ -103,26 +107,25 @@ def tm_align(dict_pu, nb_pu, input2):
         pdb_file = "results/" + str(nb_pu) + "_" + str(i) + ".pdb"
 
         # TMalign
-        CMD_LINE = ("bin/./TMalign " + pdb_file + " " + input2 + " -o TM" + str(i) + ".sup")
-        OUTPUT = call_executabe(CMD_LINE)
+        cmd_line = ("bin/./TMalign " + pdb_file + " " + input2 + " -o TM" + str(i) + ".sup")
+        output = call_executabe(cmd_line)
         # Retrieve the TM score
-        SC_TMALIGN = parse_tmscore(OUTPUT, "TMalign")
-        if SC_TMALIGN is None:
+        sc_tmalign = parse_tmscore(output, "TMalign")
+        if sc_tmalign is None:
             break
         else:
             # Keep the best score
-            if SC_TMALIGN > score[0]:
-                score[0] = SC_TMALIGN
+            if sc_tmalign > score[0]:
+                score[0] = sc_tmalign
                 score[1] = i
     # The second protein is smaller than the first so some regions cant be
     # aligned. They are write in the pdb without alignment
-    print(score[0], score[1])
-    if SC_TMALIGN is None:
+    if sc_tmalign is None:
         for i in dict_pu[nb_pu]:
             pdb_file = "results/" + str(nb_pu) + "_" + str(i) + ".pdb"
             with open(pdb_file, "r") as filin:
                 text = filin.readlines()
-            with open("test_aligned.pdb", "a") as filout:
+            with open("PU_aligned.pdb", "a") as filout:
                 for line in text:
                     filout.write(line)
     else:
@@ -133,7 +136,7 @@ def tm_align(dict_pu, nb_pu, input2):
 
 def remove_aligned_region(input, idx_pu):
     """
-
+    A REMPLIR
     """
     with open("TM" + idx_pu + ".sup_all_atm", "r") as filin:
         lines = filin.readlines()
@@ -143,7 +146,7 @@ def remove_aligned_region(input, idx_pu):
         if line[0:4] == "ATOM":
             break
 
-    with open("test_aligned.pdb", "a") as filout:
+    with open("PU_aligned.pdb", "a") as filout:
         # write the pdb PU aligned
         while lines[i][0:4].strip() != "TER":
             filout.write(lines[i])
@@ -154,7 +157,6 @@ def remove_aligned_region(input, idx_pu):
     while lines[i][0:4].strip() != "TER":
         ids.append(lines[i][30:38].strip())
         i += 1
-    k = 0
     with open(input, "r") as file_protein:
         text = file_protein.readlines()
     with open(input, "w") as file_protein:
@@ -162,5 +164,44 @@ def remove_aligned_region(input, idx_pu):
             if line[30:38].strip() not in ids:
                 file_protein.write(line)
 
-    # file_protein.truncate()
-    # file_protein.close()
+
+def main_flex_aln(input1, input2):
+    """
+    A DEFINIR
+    """
+    # os.system("dssp " + input1 + " > data/input1.dss")
+
+    # Protein peeling
+    cmd_line = ("bin/./peeling11_4.1 -pdb " + input1 +
+                " -dssp data/1aoh.dss -R2 95 -ss2 8 -lspu 20 -mspu 0 \
+                -d0 6.0 -delta 1.5 -oss 1 -p 0 -cp 0 -npu 16")
+    output = call_executabe(cmd_line).split("\n")
+    dict_pu = parse_protein_peeling(output)
+
+    with open(input2, "r") as filin:
+        protein_global = filin.readlines()
+
+    input_erasable = "results/input_erasable.pdb"
+    pu_aligned = "PU_aligned.pdb"
+    dict_tmscore = {}
+    for nb_pu in dict_pu:
+        with open(input_erasable, "w") as filout:
+            for line in protein_global:
+                filout.write(line)
+        tm_align(copy.deepcopy(dict_pu), nb_pu, input_erasable)
+        #TMscore
+        cmd_line = ("bin/./TMscore  " + pu_aligned + " " + input2 + " -o TM.sup")
+        output = call_executabe(cmd_line)
+        dict_tmscore[nb_pu] = parse_tmscore(output, "TMscore")
+        # Erase the content of the file containing the PU aligned
+        with open(pu_aligned, 'r+') as f_pu_aligned:
+            f_pu_aligned.truncate(0)
+
+    # Remove uncessary files created
+    os.system("rm TM*")
+    os.system("rm results/*.pdb")
+    os.system("rm " + pu_aligned)
+    # Display the score by the number of PU
+    for clef in dict_tmscore:
+        print("Nombre de domaines : {:>2d}  Score : {}".format(clef, dict_tmscore[clef]))
+    return dict_tmscore
